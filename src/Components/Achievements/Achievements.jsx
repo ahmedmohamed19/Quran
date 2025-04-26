cf408f0b8abacb715f151f86f7ff15e1f0cbf141
ahmedmohamed19 <ahmedmohamedahmeed6@gmail.com>
2025-04-26 04:23:43 +0300

    Update

--- src/Components/Achievements/Achievements.jsx
+++ src/Components/Achievements/Achievements.jsx
@@ -8,19 +8,19 @@ import yamn from '../../assets/yamn.jpg'
 import salah from '../../assets/salah.jpg'
 
 const achievements = [
-    { id: 1, name: 'أحمد محمود', img: ahmed, description: 'حفظ 10 أجزاء من القرآن الكريم' },
+    { id: 1, name: 'اسعد محمود', img: asad, description: 'المرتبة الأولى في مسابقة الحفظ' },
     { id: 2, name: 'علي ابراهيم', img: ali, description: 'حفظ القرآن كاملًا بتقدير ممتاز' },
-    { id: 3, name: 'اسعد محمود', img: asad, description: 'المرتبة الأولى في مسابقة الحفظ' },
-    { id: 3, name: 'يامن محمد', img: yamn, description: 'المرتبة الأولى في مسابقة الحفظ' },
-    { id: 1, name: 'أحمد محمود', img: ahmed, description: 'حفظ 10 أجزاء من القرآن الكريم' },
-    { id: 3, name: 'صلاح الدين', img: salah, description: 'المرتبة الأولى في مسابقة الحفظ' },
+    { id: 3, name: 'يامن محمد', img: yamn, description: 'حفظ 10 أجزاء من القرآن الكريم' },
+    { id: 4, name: 'أحمد محمود', img: ahmed, description: 'حفظ 10 أجزاء من القرآن الكريم' },
+    { id: 5, name: 'صلاح الدين', img: salah, description: 'حفظ 10 أجزاء من القرآن الكريم' },
+    // { id: 1, name: 'أحمد محمود', img: ahmed, description: 'حفظ 10 أجزاء من القرآن الكريم' },
 ];
 
 export default function Achievements() {
     return (
         <Container className="my-5 text-center">
             <h2 className="mb-4">إنجازات طلابنا</h2>
-            <Row>
+            <Row className='align-items-center justify-content-center'>
                 {achievements.map((achieve) => (
                     <Col key={achieve.id} md={4} className="mb-4">
                         <Card className="h-100 shadow">
